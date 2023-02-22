8addce761d291827d25a96b612793b2151577b6d
abroid <abdurroid246@gmail.com>
2023-02-22 20:16:24 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime culpa nisi modi soluta dolorum unde enim non illum, similique exercitationem dignissimos voluptatibus tempore, est quibusdam fugit excepturi reiciendis voluptates molestiae?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
