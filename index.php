9ba9c88945dd400eecee7b891b6231406fcb1c77
abroid <abdurroid246@gmail.com>
2023-02-22 20:03:47 +0700

    menambahkan paragraf di index.php

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime culpa nisi modi soluta dolorum unde enim non illum, similique exercitationem dignissimos voluptatibus tempore, est quibusdam fugit excepturi reiciendis voluptates molestiae?</p>
 </body>
 </html>
